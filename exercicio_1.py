0180f5cede40a47bb27c171b7bfa6ef305e6ee42
Lucas Vinicius Moura Costa <58613215+LucasVinicius-sudo@users.noreply.github.com>
2020-02-02 16:45:43 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,1 +1,9 @@
-
+ import pandas as pd
+ import matplotlib as plt
+ file =  pd.read_csv("C:/Users/gestor/Downloads/a2_MANCHAS(1).csv")
+ import matplotlib.pyplot as plit
+ a = list(file["Ano"])
+ b = list(file["manchas"])
+ plit.plot(a,b)
+[<matplotlib.lines.Line2D object at 0x0833F930>]
+ plit.savefig("resultado.png")
