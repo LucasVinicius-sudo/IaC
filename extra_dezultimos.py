74108f83741512c3410af9597f50ba0502aff732
Lucas Vinicius Moura Costa <58613215+LucasVinicius-sudo@users.noreply.github.com>
2020-02-02 22:15:25 -0300

    Update extra_dezultimos.py

--- extra_dezultimos.py
+++ extra_dezultimos.py
@@ -1,8 +1,8 @@
  import pandas as pd
  import matplotlib.pyplot as plt
  file =  pd.read_csv("C:/Users/gestor/Downloads/a2_MANCHAS(1).csv")
- x = list(file["Ano"])[-10]
- y = list(file["manchas"])[-10]
+ x = list(file["Ano"])[-10:]
+ y = list(file["manchas"])[-10:]
  plt.title("Qtd de manchas (10 ultimos anos)",fontsize=15,color="green",style="oblique")
  plt.plot(x,y,color="green")
  plt.xlabel("Ano",style="italic",fontsize=10)
